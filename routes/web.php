16c48fce03ff9bab89a3930e488061750e966eda
Abdurrahman <arahmanmh15@gmail.com>
2024-11-10 19:22:54 +0800

    delete task

--- routes/web.php
+++ routes/web.php
@@ -88,6 +88,11 @@ Route::get('/tasks/{task}', function (Task $task) {
     ]);
 })->name('tasks.show');
 
+Route::delete('tasks/{task}', function(Task $task){
+    $task->delete();
+
+    return redirect()->route('tasks.index')->with('success','Task deleted successfully');
+})->name('tasks.destroy');
 
 Route::fallback(function () {
     return 'Y u do this';
